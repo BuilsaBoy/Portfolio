2412eb244794f84fbbaa8641f6d64976098d6e8f
BuilsaBoy <awenjab2011@gmail.com>
2021-11-08 11:42:47 +0000

    https://github.com/BuilsaBoy/Portfolio.git

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,13 @@ import image from "../Dark Laptop.jpg"
 export default function Home(){
     return (
         <main>
-            <img src={image} alt="Dark Laptop" className="absolute object-cover display-flex" /> 
-            <section className="relative flex justify-center min-h-screen pt-8 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"> 
+            <img src={image} alt="Dark Laptop" className="absolute object-cover w-full h-full" /> 
+            <section className="relative flex justify-center text-center min-h-screen pt-8 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug"> 
                   Hello there. I'm Tony  
                 </h1>
             </section>
-            <section className="relative flex justify-center">
+            <section className="relative flex justify-center text-center sm:text-center">
                 <h2 className="text-3xl text-green-100 cursive home-sec">
                     Your Needle In the Haystack😉
                 </h2>
